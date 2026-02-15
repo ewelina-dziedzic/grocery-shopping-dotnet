1f913ae3308693797c39fff2c67d08791b616a69
Ewelina Dziedzic <ewelina.dziedzic@outlook.com>
2026-02-15 09:47:40 +0100

    Adjust model to the new API

---
+++ GroceryShopping.Infrastructure/Groceries/TodoistTasksResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GroceryShopping.Infrastructure.Groceries;
+
+public class TodoistTasksResponse
+{
+    [JsonPropertyName("results")]
+    public TodoistReadTask[] Results { get; set; } = null!;
+
+    [JsonPropertyName("next_cursor")]
+    public string NextCursor { get; set; } = null!;
+}
